5004c4d760c81d5aaa63f3748a40fbc8d8cbc17a
yanghai <yanghai@ablecloud.cn>
2018-10-18 16:01:45 +0800

    SpringCloud-Study-Init project

---
+++ eureka-server/src/main/java/com/springcloudstudy/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package com.springcloudstudy.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author yanghai
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
