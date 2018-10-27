43cae979efc8799830e4b7361c182fc0f77af9f4
yanghai <yanghai@ablecloud.cn>
2018-10-27 17:32:22 +0800

    添加日志配置文件，网页端也可以查看

--- zuul-server/src/test/java/com/springcloudstudy/zullserver/ZuulServerApplicationTests.java
+++ zuul-server/src/test/java/com/springcloudstudy/zullserver/ZuulServerApplicationTests.java
@@ -1,16 +1,19 @@
 package com.springcloudstudy.zullserver;
 
+import com.springcloudstudy.zuulserver.ZuulServerApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = ZuulServerApplication.class)
 public class ZuulServerApplicationTests {
 
     @Test
     public void contextLoads() {
+        String fileDir = System.getProperty("user.dir");
+        System.out.println(fileDir);
     }
 
 }
